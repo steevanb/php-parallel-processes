c8fe4cdc9cfc53c811d50c44d7246714cfae9dc6
Steevan BARBOYON <steevan.barboyon@gmail.com>
2021-08-26 21:38:55 +0200

    Add PHPUnit tests

---
+++ tests/Console/Application/ParallelProcessesApplication/RefreshIntervalTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Steevanb\ParallelProcess\Tests\Console\Application\ParallelProcessesApplication;
+
+use PHPUnit\Framework\TestCase;
+use Steevanb\ParallelProcess\Console\Application\ParallelProcessesApplication;
+
+class RefreshIntervalTest extends TestCase
+{
+    public function testSetRefreshInterval(): void
+    {
+        $application = new ParallelProcessesApplication();
+
+        static::assertSame(10000, $application->getRefreshInterval());
+
+        $application->setRefreshInterval(12);
+
+        static::assertSame(12, $application->getRefreshInterval());
+    }
+}
