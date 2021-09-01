4515f3c0b414a6485e0658cbe52fa4004699da20
Steevan BARBOYON <steevan.barboyon@gmail.com>
2021-09-01 20:46:38 +0200

    Release with GitHub Actions

--- bin/release/phpunit-coverage-percent.php
+++ bin/release/phpunit-coverage-percent.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 try {
-    $xml = simplexml_load_file(__DIR__ . '/../../var/ci/phpunit/coverage/xml/index.xml');
+    $xml = simplexml_load_file(__DIR__ . '/../../var/release/phpunit/coverage/xml/index.xml');
     foreach ($xml->project->directory as $directory) {
         if ((string) $directory->attributes()['name'] === '/') {
             echo ceil((float) $directory->totals->lines->attributes()['percent']);
