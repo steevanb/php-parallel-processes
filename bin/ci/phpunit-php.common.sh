a13a830dc28bda6c8cf7d5bef2c0d1a2f4d497ae
Steevan BARBOYON <steevan.barboyon@gmail.com>
2021-09-06 18:11:29 +0200

    Remove container in GitHub Actions configuration

--- bin/ci/phpunit-php.common.sh
+++ bin/ci/phpunit-php.common.sh
@@ -2,22 +2,23 @@
 
 set -eu
 
-if type docker > /dev/null 2>&1; then
+if ! ${IS_IN_DOCKER}; then
     docker \
         run \
-            -it \
-            --tty \
             --rm \
+            --tty \
+            ${DOCKER_INTERACTIVE_PARAMETER} \
             --volume "${ROOT_DIR}":/app \
             --user "$(id -u)":"$(id -g)" \
             --entrypoint bin/ci/"$(basename "${0}")" \
             --workdir /app \
             steevanb/php-parallel-process:ci \
             "${@}"
-else
-    "${PHP_BIN}" \
-        vendor/bin/phpunit \
-            --bootstrap "${COMPOSER_HOME_SYMFONY}"/vendor/autoload.php \
-            --configuration config/ci/phpunit.xml \
-            "${@}"
+    exit 0
 fi
+
+"${PHP_BIN}" \
+    vendor/bin/phpunit \
+        --bootstrap "${COMPOSER_HOME_SYMFONY}"/vendor/autoload.php \
+        --configuration config/ci/phpunit.xml \
+        "${@}"
