e4389444a5dcf43cf2e9722620f8949833b95184
Steevan BARBOYON <steevan.barboyon@gmail.com>
2022-01-08 11:30:29 +0100

    Prepare release 0.2.1

--- bin/common.inc.bash
+++ bin/common.inc.bash
@@ -4,4 +4,4 @@ set -eu
 
 readonly CI_DOCKER_IMAGE_NAME="steevanb/php-parallel-processes:ci"
 readonly RELEASE_DOCKER_IMAGE_NAME="steevanb/php-parallel-processes:release"
-readonly PARALLEL_PROCESSES_DOCKER_IMAGE_NAME="steevanb/php-parallel-processes:parallel-processes-0.2.0"
+readonly PARALLEL_PROCESSES_DOCKER_IMAGE_NAME="steevanb/php-parallel-processes:parallel-processes-0.2.1"
