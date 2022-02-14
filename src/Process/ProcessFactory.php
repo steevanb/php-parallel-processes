fa872124f0d77de414e1d01dab06b4640cd0eb8c
Steevan BARBOYON <steevan.barboyon@gmail.com>
2022-02-14 14:40:41 +0100

    Add ProcessFactory::createRemoveFileProcess()

--- src/Process/ProcessFactory.php
+++ src/Process/ProcessFactory.php
@@ -5,25 +5,52 @@ declare(strict_types=1);
 namespace Steevanb\ParallelProcess\Process;
 
 use Steevanb\ParallelProcess\Console\Application\ParallelProcessesApplication;
+use steevanb\PhpTypedArray\ScalarArray\StringArray;
 
 class ProcessFactory
 {
     public static function createRemoveDirectoryProcess(
-        string $directory,
+        string $path,
         ParallelProcessesApplication $application,
         string $name = null,
         string $cwd = null
     ): ?Process {
-        if (is_dir($directory)) {
-            $return = new Process(['rm', '-rf', $directory], $cwd);
-            if (is_string($name)) {
-                $return->setName($name);
-            }
-
-            $application->addProcess($return);
+        if (is_dir($path)) {
+            $return = static::createProcess(new StringArray(['rm', '-rf', $path]), $application, $name, $cwd);
         } else {
             $return = null;
         }
+
+        return $return;
+    }
+
+    public static function createRemoveFileProcess(
+        string $filePathname,
+        ParallelProcessesApplication $application,
+        string $name = null,
+        string $cwd = null
+    ): ?Process {
+        if (is_file($filePathname)) {
+            $return = static::createProcess(new StringArray(['rm', $filePathname]), $application, $name, $cwd);
+        } else {
+            $return = null;
+        }
+
+        return $return;
+    }
+
+    protected static function createProcess(
+        StringArray $command,
+        ParallelProcessesApplication $application,
+        string $name = null,
+        string $cwd = null
+    ): Process {
+        $return = new Process($command->toArray(), $cwd);
+        if (is_string($name)) {
+            $return->setName($name);
+        }
+
+        $application->addProcess($return);
 
         return $return;
     }
